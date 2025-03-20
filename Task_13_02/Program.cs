7cdc2520d918a8c005898d151bec27d286b5e869
Яна Кауркина <kauryan_11@mail.ru>
2025-03-20 14:02:54 +0300

    решено

---
+++ Task_13_02/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace Task_13_02
+{
+    /*создать класс питомца
+свойства:
+кличка, вид животного, возраст, вес, отметка о состонии здоровья (здоров/нездоров)
+методы:
+вывод информации об объекте
+изменение значения веса животного
+изменение отметки о состоянии здоровья
+конструторы:
+предусмотрите разные варианты инициализации объектов*/
+    using System;
+
+    public class Pet
+    {
+#region поля
+        public string Nickname { get; set; }
+        public string Species { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public bool IsHealthy { get; set; }
+        #endregion 
+
+        #region конструкторы
+        public Pet()
+        {//инициализация всего
+            Nickname = "Безымянный";
+            Species = "Неизвестный вид";
+            Age = 0;
+            Weight = 1.0;
+            IsHealthy = true;
+        }
+        public Pet(string nickname, string species) : this() //кличка и вид
+        {
+            Nickname = nickname;
+            Species = species;
+        }
+        public Pet(string nickname, string species, int age, double weight) : this(nickname, species) // всё кроме здоровья
+        {
+            Age = age;
+            Weight = weight;
+        }
+
+        public Pet(string nickname, string species, int age, double weight, bool isHealthy) : this(nickname, species, age, weight) // все параметры
+        {
+            IsHealthy = isHealthy;
+        }
+        #endregion
+
+        #region методы
+
+        //вывод
+        public void GetPetInfo()
+        {
+            Console.WriteLine("--- Информация о питомце ---");
+            Console.WriteLine($"Кличка: {Nickname}");
+            Console.WriteLine($"Вид: {Species}");
+            Console.WriteLine($"Возраст: {Age} лет");
+            Console.WriteLine($"Вес: {Weight} кг");
+            Console.WriteLine($"Состояние здоровья: {(IsHealthy ? "Здоров" : "Не здоров")}");
+            Console.WriteLine("--------------------------");
+        }
+
+        //изменение веса
+        public void SetWeight(double newWeight)
+        {
+            if (newWeight > 0)
+            {
+                Weight = newWeight;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимый вес. Вес должен быть больше нуля.");
+            }
+        }
+        #endregion 
+    }
+}
---
+++ Task_13_03/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Task_13_03
+{
+    /*создайте класс автомобиля
+свойства:
+номер авто, марка, цвет, текущая скорость
+методы:
+езда (симитировать равномерное ускорение скорости автомобиля)
+торможение (при превышении скорости автомобиля свыше допустимой - он должен остановиться)
+конструторы
+предусмотрите разные варианты инициализации объектов*/
+    using System;
+
+    public class Car
+    {
+        #region поля
+        public string LicensePlate { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public double CurrentSpeed { get; set; } //изменение скорости
+        public double MaxSpeed { get; set; }  //наивысшая скорость
+        #endregion
+       
+        private const double DefaultMaxSpeed = 200;//макс.скорость
+
+#region конструкторы
+
+        public Car()
+        {
+            LicensePlate = "Не указан";
+            Brand = "Неизвестно";
+            Color = "Не определен";
+            CurrentSpeed = 0;
+            MaxSpeed = DefaultMaxSpeed;
+        }
+
+
+        public Car(string licensePlate, string brand) : this()
+        {
+            LicensePlate = licensePlate;
+            Brand = brand;
+        }
+
+
+        public Car(string licensePlate, string brand, string color) : this(licensePlate, brand)
+        {
+            Color = color;
+        }
+
+        public Car(string licensePlate, string brand, string color, double currentSpeed) : this(licensePlate, brand, color)
+        {
+            CurrentSpeed = currentSpeed;
+        }
+        public Car(string licensePlate, string brand, string color, double currentSpeed, double maxSpeed) : this(licensePlate, brand, color, currentSpeed)
+        {
+            MaxSpeed = maxSpeed;
+        }
+        #endregion
+#region методы
+         //ускорение
+        public void Accelerate(double acceleration, double time)
+        {
+            if (acceleration <= 0 || time <= 0)
+            {
+                Console.WriteLine("Ускорение и время должны быть положительными.");
+                return;
+            }
+
+            double newSpeed = CurrentSpeed + acceleration * time;
+
+            if (newSpeed > MaxSpeed)
+            {
+                CurrentSpeed = MaxSpeed;
+                Console.WriteLine($"Достигнута максимальная скорость ({MaxSpeed} км/ч).");
+            }
+            else
+            {
+                CurrentSpeed = newSpeed;
+                Console.WriteLine($"Скорость увеличена до {CurrentSpeed} км/ч.");
+            }
+        }
+
+        //торможение
+        public void Brake(double speedLimit)
+        {
+            if (CurrentSpeed > speedLimit)
+            {
+                Console.WriteLine($"Превышена допустимая скорость ({speedLimit} км/ч). Экстренное торможение!");
+                CurrentSpeed = 0;
+            }
+            else
+            {
+                Console.WriteLine("Торможение прошло успешно.");
+            }
+        }
+        #endregion
+        //вывод информации
+        public void GetCarInfo()
+        {
+            Console.WriteLine("--- Информация об автомобиле ---");
+            Console.WriteLine($"Номер: {LicensePlate}");
+            Console.WriteLine($"Марка: {Brand}");
+            Console.WriteLine($"Цвет: {Color}");
+            Console.WriteLine($"Текущая скорость: {CurrentSpeed} км/ч");
+            Console.WriteLine($"Максимальная скорость: {MaxSpeed} км/ч");
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
