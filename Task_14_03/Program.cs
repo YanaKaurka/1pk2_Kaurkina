c03c2a6a4c3ed09f423d741b2f4c7bea87146728
Яна Кауркина <kauryan_11@mail.ru>
2025-03-20 12:37:24 +0300

    домашка решена

---
+++ Task_14_03/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_14_03
+{
+    /*Реализуйте статический метод Factorial, который принимает целое число и возвращает его факториал. Сделайтетак, чтобы метод работал только для неотрицательных чисел.*/
+    internal class Program
+    {
+        public static long Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Факториал определен только для неотрицательных чисел.");
+            }
+
+            if (n == 0)
+            {
+                return 1; // Факториал 0 равен 1
+            }
+
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Task_14_04/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_14_04
+{
+    /*Определите класс User, который будет иметь статическое свойство CurrentUser, представляющеетекущегопользователя, 
+     * и метод для его установки.*/
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        // Конструктор (для создания объектов User)
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+        
+        public void GetUserInfo()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Username: {Username}");
+            //Console.WriteLine($"Password: {Password}"); // Не выводите пароль!
+        }
+    }
+
+    
+    
+
+}
