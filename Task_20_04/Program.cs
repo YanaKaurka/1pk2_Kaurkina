6d884875179690f03c9626c2af43cee873daa950
Яна Кауркина <kauryan_11@mail.ru>
2025-04-11 19:31:00 +0300

    20.04 сделано

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Task_20_04
+{
+    /*Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+• Car
+• Bike
+• Bus
+• Truck
+• Motorcycle
+Храните список транспортных средств (можно просто List<VehicleType>). 
+    Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков).
+    Реализуйте поиск по типу и вывод информации.*/
+
+
+    namespace VehicleManagement
+    {
+        // Перечисление для типов транспортных средств
+        public enum VehicleType
+        {
+            Car,
+            Bike,
+            Bus,
+            Truck,
+            Motorcycle
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                VehicleManager manager = new VehicleManager();
+
+                // Добавление транспортных средств
+                manager.AddVehicle(new Vehicle(VehicleType.Car, "Toyota Camry"));
+                manager.AddVehicle(new Vehicle(VehicleType.Bike, "Yamaha YZF-R3"));
+                manager.AddVehicle(new Vehicle(VehicleType.Bus, "Mercedes-Benz Sprinter"));
+                manager.AddVehicle(new Vehicle(VehicleType.Truck, "Volvo FH"));
+                manager.AddVehicle(new Vehicle(VehicleType.Motorcycle, "Kawasaki Ninja"));
+
+                // Подсчет грузовиков
+                int truckCount = manager.CountVehiclesByType(VehicleType.Truck);
+                Console.WriteLine($"Количество грузовиков: {truckCount}");
+
+                // Вывод информации о мотоциклах
+                manager.PrintVehiclesByType(VehicleType.Motorcycle);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+    
+
---
+++ Task_20_04/Vehicle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_20_04.VehicleManagement;
+
+namespace Task_20_04
+{
+    public class Vehicle
+    {
+        public VehicleType Type { get; set; }
+        public string Model { get; set; }
+
+        public Vehicle(VehicleType type, string model)
+        {
+            Type = type;
+            Model = model;
+        }
+        public override string ToString()
+        {
+            return $"{Type} - {Model}";
+        }
+    }
+    public class VehicleManager
+    {
+        private List<Vehicle> vehicles;
+
+        public VehicleManager()
+        {
+            vehicles = new List<Vehicle>();
+        }
+
+        // Метод для добавления транспортного средства в список
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        // Метод для подсчета транспорта определенного типа
+        public int CountVehiclesByType(VehicleType type)
+        {
+            int count = 0;
+            foreach (var vehicle in vehicles)
+            {
+                if (vehicle.Type == type)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        // Метод для поиска и вывода информации 
+        public void PrintVehiclesByType(VehicleType type)
+        {
+            Console.WriteLine($"Транспортные средства типа {type}:");
+            foreach (var vehicle in vehicles)
+            {
+                if (vehicle.Type == type)
+                {
+                    Console.WriteLine(vehicle);
+                }
+            }
+        }
+
+    }
+}
